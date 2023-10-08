b0f2c20fced6242887cc5a656bc00514ee689872
Dvimin <dmitriev.michail@mail.ru>
2023-10-08 03:37:45 +0300

    Operation

--- Source.cpp
+++ Source.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include <math.h>
+#include <map>
+#include <string>
 
 class Operation {
 public:
-	virtual double perform(double leftOperand, double rightOperand) const = 0;
+
+	virtual double perform(double leftOperand, double rightOperand) const { return INFINITY; };
+	virtual double perform(double Operand) const { return INFINITY; };
+};
+
+class SinFunction : public Operation {
+public:
+	double perform(double Operand) const override {
+		return sin(Operand);
+	}
 };
 
 class Addition : public Operation {
@@ -31,27 +42,24 @@ public:
 		return leftOperand / rightOperand;
 	}
 };
-
-class Function {
-public:
-	virtual double fun(double argument) const = 0;
-};
-
-class SinFunction : public Function {
-public:
-	double fun(double argument) const override {
-		return sin(argument);
-	}
-};
-
-class CosFunction : public Function {
-public:
-	double fun(double argument) const override {
-		return cos(argument);
-	}
-};
-
 int main() {
+	
+	Operation* plus = new Addition();
+	Operation* minus = new Subtraction();
+	Operation* sin = new SinFunction();
+	Operation* division = new Division();
+
+	std::map<std::string, Operation*> operations;
+	operations["+"] = plus;
+	operations["-"] = minus;
+	operations["/"] = division;
+	operations["sin"] = sin;
+
+	std::cout << plus->perform(10, 3) << " " << minus->perform(10, 3) << std::endl;
+	std::cout << operations["-"]->perform(10, 3) << std::endl;
+	std::cout << operations["sin"]->perform(3.14) << std::endl;
+	std::cout << operations["sin"]->perform(3.14,8) << std::endl;
+	std::cout << operations["/"]->perform(-8, 0) << std::endl;
 
 	return 0;
 }
